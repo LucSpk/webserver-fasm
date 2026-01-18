ec2c3c732487c4c4aee70c26cf9273ee2e57e63b
Lucas Alves <olalvezluc@gmail.com>
2026-01-17 22:53:56 -0300

    Adiciona programa principal

---
+++ website.asm
@@ -0,0 +1,30 @@
+format ELF64 executable
+
+SYS_write = 1
+SYS_exite = 60
+
+macro write fd, buf, count {
+    mov rax, SYS_write
+    mov rdi, fd
+    mov rsi, buf
+    mov rdx, count
+    syscall
+}
+
+macro exit code {
+    mov rax, SYS_exite
+    mov rdi, code
+    syscall
+}
+
+segment readable executable
+entry main
+
+main: 
+    
+    write 1, start, start_len
+    exit 0
+
+segment readable writeable
+start db "Starting Web Server!", 10
+start_len = $ - start
