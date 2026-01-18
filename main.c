1db6807ffb53fd328a841e4d50af6dcc867d4950
Lucas Alves <olalvezluc@gmail.com>
2026-01-18 00:03:16 -0300

    Descobre SOCK_STREAM com o programa em C

--- main.c
+++ main.c
@@ -4,5 +4,6 @@
 int main(void) {
     // Usar o comando cc -o main main.c && ./main para rodar
     printf("AF_INET = %d\n", AF_INET);
+    printf("SOCK_STREAM = %d\n", SOCK_STREAM);
     return 0;
 }
