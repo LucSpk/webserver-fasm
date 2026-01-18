f9e6b1afdafbf47806b5859a7d549eb37baf91b6
Lucas Alves <olalvezluc@gmail.com>
2026-01-18 00:05:02 -0300

    Cria socket

--- website.asm
+++ website.asm
@@ -2,6 +2,10 @@ format ELF64 executable
 
 SYS_write = 1
 SYS_exite = 60
+SYS_socket = 41
+
+AF_INET = 2
+SOCK_STREAM = 1
 
 macro write fd, buf, count {
     mov rax, SYS_write
@@ -9,6 +13,15 @@ macro write fd, buf, count {
     mov rsi, buf
     mov rdx, count
     syscall
+}
+
+macro socket domain, type, protocol 
+{
+    MOV rax, SYS_socket
+    MOV rdi, domain
+    MOV rsi, type
+    MOV rdx, protocol
+    SYSCALL
 }
 
 macro exit code {
@@ -23,6 +36,8 @@ entry main
 main: 
     
     write 1, start, start_len
+    socket AF_INET, SOCK_STREAM, 0
+
     exit 0
 
 segment readable writeable
