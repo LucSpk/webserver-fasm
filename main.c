6dfc3500642030047f3c7ab28d570b4c252ae384
Lucas Alves <olalvezluc@gmail.com>
2026-01-18 00:00:59 -0300

    Descubrir o AF_INET com um programa c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+int main(void) {
+    // Usar o comando cc -o main main.c && ./main para rodar
+    printf("AF_INET = %d\n", AF_INET);
+    return 0;
+}
