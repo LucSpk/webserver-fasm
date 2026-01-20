9acbe742d9f4875824eb61e771df2d98bcd2385a
Lucas Alves <olalvezluc@gmail.com>
2026-01-20 19:51:22 -0300

    Verifica valro da constante INADDR_ANY

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
 
 int main(void) {
     // Usar o comando cc -o main main.c && ./main para rodar
     printf("AF_INET = %d\n", AF_INET);
     printf("SOCK_STREAM = %d\n", SOCK_STREAM);
+    printf("INADDR_ANY = %d\n", INADDR_ANY);
+    
     return 0;
 }
