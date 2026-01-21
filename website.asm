b52b48fc0fe3d021b7b4d5e08b618ed740e9d27b
Lucas Alves <olalvezluc@gmail.com>
2026-01-21 20:01:36 -0300

    binding the socket

--- website.asm
+++ website.asm
@@ -3,15 +3,18 @@ format ELF64 executable
 SYS_write equ 1
 SYS_exite equ 60
 SYS_socket equ 41
+SYS_bind equ 49
 
 AF_INET equ 2
 SOCK_STREAM equ 1
+INADDR_ANY = 0
 
 STDOUT equ 1
 STDERR equ 2
 
 EXIT_SUCCESS equ 0
 EXIT_FAILURE equ 1
+
 
 macro write fd, buf, count {
     mov rax, SYS_write
@@ -28,6 +31,19 @@ macro socket domain, type, protocol
     MOV rsi, type
     MOV rdx, protocol
     SYSCALL
+}
+macro syscall3 number, a, b, c 
+{
+    mov rax, number
+    mov rdi, a
+    mov rax, b
+    mov rax, c
+    syscall
+}
+
+macro bind sockfd, addr, addrlen
+{
+    syscall3 SYS_bind, sockfd, addr, addrlen
 }
 
 macro exit code {
@@ -48,7 +64,17 @@ main:
     cmp rax, 0
     jl error
 
-    mov dword [sockfd], eax
+    mov qword [sockfd], rax
+
+    write STDOUT, bind_trace_msg, bind_trace_msg_len
+    mov word [servaddr.sin_family], AF_INET
+    mov word [servaddr.sin_port], 14619         ;; Isso corresponde a porta 6969 -> o valor 6969 tem 2 bytes que quando invertido da 14619. Note que 6969 em hexa é 0x1b39 e seu inverso 0x391b é 14619
+    mov dword [servaddr.sin_addr], INADDR_ANY
+
+    bind [sockfd], servaddr.sin_family, servaddr.sin_addr
+    cmp rax, 0
+    jl error
+ 
     write STDOUT, ok_msg, ok_msg_len
 
     exit EXIT_SUCCESS
@@ -58,7 +84,12 @@ error:
     exit EXIT_FAILURE
 
 segment readable writeable
-sockfd dd 0
+sockfd dq 0
+servaddr.sin_family dw 0
+servaddr.sin_port dw 0
+servaddr.sin_addr dd 0
+servaddr.sin_zero dq 0
+sizeof_servaddr = $ - servaddr.sin_family
 
 start db "INFO: Starting Web Server!", 10
 start_len = $ - start
@@ -68,6 +99,9 @@ ok_msg_len = $ - ok_msg
 
 socket_trace_msg db "INFO: Creating a socket...", 10
 socket_trace_msg_len = $ - socket_trace_msg
+
+bind_trace_msg db "INFO: Binding the socket...", 10
+bind_trace_msg_len = $ - bind_trace_msg
 
 error_msg db "ERROR!", 10
 error_msg_len = $ - error_msg
